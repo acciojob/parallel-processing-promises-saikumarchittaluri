0fd16c7590fe7d790949acd8ae479debe5457418
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-09-01 15:36:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -7,3 +6,41 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+// Function to download an image given its URL
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const imgElement = new Image();
+    imgElement.src = image.url;
+
+    imgElement.onload = () => {
+      resolve(imgElement);
+    };
+
+    imgElement.onerror = () => {
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    };
+  });
+}
+
+// Function to display downloaded images
+function displayImages(images) {
+  output.innerHTML = ''; // Clear any existing content
+
+  images.forEach((img) => {
+    output.appendChild(img);
+  });
+}
+
+// Event listener for the download button
+btn.addEventListener("click", () => {
+  const promises = images.map((image) => downloadImage(image));
+
+  Promise.all(promises)
+    .then((downloadedImages) => {
+      displayImages(downloadedImages);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
